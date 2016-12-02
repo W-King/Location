1d8a8797d019bcc169028ef9abc223956d809521
jiboxue <jibx@ichsy.com>
2016-12-02 14:47:04 +0800

    Initial Commit

---
+++ PPX_LocationTests/PPX_LocationTests.m
@@ -0,0 +1,39 @@
+//
+//  PPX_LocationTests.m
+//  PPX_LocationTests
+//
+//  Created by pipixia on 2016/12/2.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PPX_LocationTests : XCTestCase
+
+@end
+
+@implementation PPX_LocationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
